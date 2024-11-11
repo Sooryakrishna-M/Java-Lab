9c5d01a6dc3e5dc54c3f343d1df65c5492b52beb
Sooryakrishna M <sk2004international@gmail.com>
2024-11-11 12:25:03 +0530

    File created for String Tokenizer

---
+++ stringtok.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class stringtok {
+    public static void main (String args[]){
+    int n;
+    int sum=0;
+
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the String with space gaps ");
+    String s=sc.nextLine();
+    StringTokenizer str=new StringTokenizer(s," ");
+
+    while(str.hasMoreTokens()){
+        String temp=str.nextToken();
+        n=Integer.parseInt(temp);
+        System.out.println(n);
+        sum=sum+n;
+
+    }
+    System.out.println("The sum is "+sum);
+    
+}
+}
